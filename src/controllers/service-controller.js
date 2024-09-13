08a8586e75a31e20c9b62923bdb96d75d045912d
emeraldachecia <emeraldachecia@gmail.com>
2024-09-13 15:58:13 +0700

    service page

---
+++ src/controllers/service-controller.js
@@ -0,0 +1,13 @@
+export const service_page = async (req, res) => {
+    try {
+        res.render("service", {
+            layout: "layouts/main",
+            title: "Reservasi Bali Halus",
+            script: "service-script.js",
+            style: "service-style.css",
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(error.status || 500).json({ error: error.message });
+    }
+};
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import { home_page } from "./controllers/home-controller.js";
 import { reservation_page } from "./controllers/reservation-controller.js";
 import { history_page } from "./controllers/history-controller.js";
+import { service_page } from "./controllers/service-controller.js";
 
 const router = express.Router();
 
 router.get("/", home_page);
 router.get("/reservation", reservation_page);
 router.get("/history", history_page)
+router.get("/service", service_page)
 
 export default router;
